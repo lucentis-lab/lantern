f3bade5c381153110eb4b7615768a985594cc894
lucentis <lucentis.dev@gmail.com>
2026-01-09 11:01:45 +0100

    made types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,61 @@
-export interface PluginOptions {
-  theme: Theme
-}
+import type { HTMLAttributes } from 'vue'
 
 export interface Theme {
-  colors: string
+  colors: ThemeColors
+  size: ThemeSizes
+  radius: ThemeRadius
+  [propName: string]: Record<string, unknown> // for other theme props
+}
+
+// Component Spec
+export interface ComponentSpec {
+  name: string
+  apply?: string[]
+  class?: string
+  defaultProps?: {
+    color?: string
+    variant?: string
+    size?: string
+    radius?: string
+    [key: string]: string | undefined
+  }
+  override?: {
+    colors?: ThemeColors
+    size?: ThemeSizes
+    radius?: ThemeRadius
+    [propName: string]: Record<string, unknown> | undefined // size, radius, etc.
+  }
+}
+
+// Plugin options (no more defaultColor/defaultVariant)
+export interface PluginOptions {
+  theme: Theme
+  defaultColor?: string
+  defaultVariant?: string
+}
+
+export interface BaseProps {
+  class?: HTMLAttributes['class']
+  color?: string
+  variant?: string
+}
+
+export interface ComponentProps extends BaseProps {
+  [propName: string]: unknown
+}
+
+export interface ThemeColors {
+  [colorName: string]: {
+    [variantName: string]: {
+      [key: string]: string
+    }
+  }
+}
+
+export interface ThemeSizes {
+  [sizeName: string]: string
+}
+
+export interface ThemeRadius {
+  [radiusName: string]: string
 }
