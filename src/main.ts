4161f3a26a9f5d45ce7c50dc923f8544635ffb8d
Lucentis <lucentis.dev@gmail.com>
2026-03-03 09:14:54 +0000

    add tailwind for test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './style.css'
 
 import { lantern } from './plugin'
 import { defaultTheme, createTheme } from './themes'
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL, resolve } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
